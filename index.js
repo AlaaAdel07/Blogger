e5e8f688a9a037389ffe1f9f8555f4fec3fb44e7
AlaaAdel07 <58051696+AlaaAdel07@users.noreply.github.com>
2021-06-12 04:43:55 +0200

    edit2

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.get('/',(eq, res)=>{
         res.statusCode = 422;
         res.send(error);
     }
-}
+});
 
 app.use((req, res, next) => {
     console.log(`Request Url : ${req.url}, Request method : ${req.method}, Date of Request: ${Date()}`);
